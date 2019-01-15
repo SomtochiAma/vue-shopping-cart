56ac14377c341ad26af78f63464aee002986c041
Somto Onyekwere <somtochionyekwere@gmail.com>
2019-01-15 16:41:46 +0100

    Feat: Remove button on all products table working now

--- main.js
+++ main.js
@@ -22,13 +22,6 @@ Vue.component('form-elem', {
         }
     },
 
-    methods: {
-        handleInput() {
-            this.$emit('input', this.content)
-        }
-    },
-
-
     computed: {
         id() {
             return this.name.toLowerCase().replace(/ /g, '-')
@@ -38,6 +31,48 @@ Vue.component('form-elem', {
 
 })
 
+Vue.component('product-table', {
+    template: `
+        <table class="table table-dark" id="allProducts">
+                <thead>
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Description</th>
+                        <th scope="col">Quantitiy</th>
+                        <th scope="col">Price</th>
+                        <th scope="col">Actions</th>
+                    </tr>
+                </thead>
+                <slot></slot>
+        </table>
+    `
+})
+
+
+Vue.component('product', {
+
+
+    template: `
+        <tbody>
+            <tr v-for="product in events">
+                <td>{{ product.name}}</td>
+                <td>{{ product.desc}}</td>
+                <td>{{ product.quantity}}</td>
+                <td>{{ product.price}}</td>
+                <td><button class="btn btn-danger removeBtn" @click="deleteProduct(product.name)">REMOVE</button><button class="btn btn-primary">ADD TO CART</button></td>
+            </tr>
+        </tbody>
+    `,
+
+    props: ['events'],
+
+    
+    methods: {
+        deleteProduct(productName) {
+            this.$emit('remove-product', productName)
+        }
+    }
+})
 
 new Vue({
     el: "#root",
@@ -49,13 +84,28 @@ new Vue({
             quantity: '',
             price: '',
         },
+
+        addedProducts: [],
     },
 
     methods: {
         giveProductInfo() {
-            console.log(this.product)
+            console.log(this.product);
+            let newProduct = {
+                name: this.product.name,
+                desc: this.product.desc,
+                quantity: this.product.quantity,
+                price: this.product.price,
+            }
+
+            console.log(newProduct);
+            this.addedProducts.push(newProduct)
+            
+        },
+
+        deleteProduct(productName) {
+            this.addedProducts = this.addedProducts.filter( product => product.name !== productName)
         }
     }
-
 
 })
